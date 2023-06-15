c48c20109cf6f2bc74969f108ada41b2b40d5b9c
Helal Anwar <hilal123anwar@gmail.com>
2023-06-15 11:29:59 +0530

    Add custom layout MenuTextFieldLayout

--- app/src/main/java/org/helal/test/ui/MenuTextFieldLayout.java
+++ app/src/main/java/org/helal/test/ui/MenuTextFieldLayout.java
@@ -1,9 +1,11 @@
 package org.helal.test.ui;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.widget.LinearLayoutCompat;
@@ -16,6 +18,7 @@ public class MenuTextFieldLayout extends LinearLayout {
 
     private TextInputEditText textInputEditText;
     private LinearLayoutCompat linearLayout;
+    private TextView textView;
 
     public MenuTextFieldLayout(Context context) {
         super(context);
@@ -23,20 +26,39 @@ public class MenuTextFieldLayout extends LinearLayout {
 
     public MenuTextFieldLayout(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.MenuTextFieldLayout, 0, 0);
+        String titleText = a.getString(R.styleable.MenuTextFieldLayout_titleText);
+        int focus_color = a.getColor(R.styleable.MenuTextFieldLayout_focusColorValue,
+                getResources().getColor(android.R.color.holo_blue_light, context.getTheme()));
+        int label_text_fill = a.getColor(R.styleable.MenuTextFieldLayout_labelTextFillColor,
+                getResources().getColor(android.R.color.holo_blue_light, context.getTheme()));
+        a.recycle();
         LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         inflater.inflate(R.layout.menu_text_editor, this, true);
         linearLayout = findViewById(R.id.menu_text_main);
         textInputEditText = findViewById(R.id.textInputEditText);
-        textInputEditText.setOnClickListener(v -> {
+        textView = findViewById(R.id.title_lable);
+        textInputEditText.setFocusableInTouchMode(true);
+        textInputEditText.setOnFocusChangeListener((v, hasFocus) -> actionOnFocus(hasFocus, focus_color, label_text_fill));
 
-        });
+    }
+
+    public MenuTextFieldLayout(Context context, AttributeSet attrs, int defStyleAttr,
+                               int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
     }
 
     public MenuTextFieldLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
-    public MenuTextFieldLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
+    private void actionOnFocus(boolean hasFocus, int focus_color, int label_text_fill) {
+        if (hasFocus) {
+            linearLayout.setBackgroundResource(R.drawable.menu_textfield_selected);
+            textView.setTextColor(focus_color);
+        } else {
+            linearLayout.setBackgroundResource(R.drawable.menu_textfield_normal);
+            textView.setTextColor(label_text_fill);
+        }
     }
 }
